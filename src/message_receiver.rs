e750a005f57d47c3b0c2eb798c6a77895440b11e
Tor Arvid Lund <torarvid@gmail.com>
2023-05-24 21:39:08 +0200

    Start work on efficient broadcast part 1

--- src/message_receiver.rs
+++ src/message_receiver.rs
@@ -16,6 +16,14 @@ pub struct MessageReceiver {
     msg_queue: Arc<RwLock<HashMap<usize, Message>>>,
 }
 
+fn now_us() -> u128 {
+    let time = std::time::SystemTime::now();
+    let since_the_epoch = time
+        .duration_since(std::time::UNIX_EPOCH)
+        .expect("Time went backwards");
+    since_the_epoch.as_micros()
+}
+
 impl MessageReceiver {
     pub fn new(repo: Box<dyn Repo>) -> Self {
         let mut instance = Self {
@@ -39,11 +47,12 @@ impl MessageReceiver {
     }
 
     fn process_incoming(&mut self, message: Message) {
+        eprintln!("{}: Received message: {:?}", now_us(), message);
         if let Some(body_type) = match message.body.typ.clone() {
             BodyType::Echo(body) => Some(BodyType::EchoOk(body)),
             BodyType::Init(body) => Some(self.on_init(body)),
             BodyType::Generate => Some(BodyType::GenerateOk(BodyGenerate::new())),
-            BodyType::Broadcast(body) => self.on_broadcast(body),
+            BodyType::Broadcast(body) => self.on_broadcast(body, message.clone()),
             BodyType::BroadcastOk => self.on_broadcast_ok(&message.body),
             BodyType::Read => self.on_read(),
             BodyType::Topology(body) => Some(self.on_topology(body)),
@@ -59,6 +68,7 @@ impl MessageReceiver {
 
     fn send_outgoing(message: &Message) {
         println!("{}", serde_json::to_string(message).unwrap());
+        eprintln!("{}: Sent message: {:?}", now_us(), message);
     }
 
     fn get_next_msg_id() -> usize {
@@ -67,13 +77,23 @@ impl MessageReceiver {
 
     fn queue_retrier(&mut self) {
         let msg_queue_lock = Arc::clone(&self.msg_queue);
-        thread::spawn(move || loop {
-            if let Ok(msq_queue) = msg_queue_lock.read() {
-                for (_msg_id, message) in msq_queue.iter() {
-                    MessageReceiver::send_outgoing(message);
+        thread::spawn(move || {
+            let mut retry_number = 0;
+            loop {
+                let mut msg_vec = Vec::new();
+                {
+                    if let Ok(msg_queue) = msg_queue_lock.read() {
+                        for message in msg_queue.values() {
+                            msg_vec.push(message.clone());
+                        }
+                    }
                 }
-            };
-            thread::sleep(std::time::Duration::from_millis(100));
+                for message in msg_vec {
+                    MessageReceiver::send_outgoing(&message);
+                }
+                retry_number += 1;
+                thread::sleep(std::time::Duration::from_millis(1000 * retry_number));
+            }
         });
     }
 
@@ -83,33 +103,61 @@ impl MessageReceiver {
         BodyType::InitOk
     }
 
-    fn on_broadcast(&mut self, body_broadcast: BodyBroadcast) -> Option<BodyType> {
-        let neighbors: Vec<Node> = self.repo.all_nodes().into_iter().cloned().collect();
+    fn on_broadcast(&mut self, body_broadcast: BodyBroadcast, source: Message) -> Option<BodyType> {
+        eprintln!("{}:   Received broadcast: {:?}", now_us(), body_broadcast);
+        let message = Message {
+            src: source.dest.clone(),
+            dest: source.src.clone(),
+            body: Body {
+                typ: BodyType::BroadcastOk,
+                msg_id: Some(MessageReceiver::get_next_msg_id()),
+                in_reply_to: source.body.msg_id,
+            },
+        };
+        MessageReceiver::send_outgoing(&message);
 
-        if let Some(node) = self.repo.this_node() {
-            if !node.has_message(body_broadcast.message) {
-                node.add_message(body_broadcast.message);
-                for neighbor_node in neighbors {
-                    let msg_id = MessageReceiver::get_next_msg_id();
-                    let message = Message {
-                        src: node.id(),
-                        dest: neighbor_node.id(),
-                        body: Body {
-                            typ: BodyType::Broadcast(BodyBroadcast {
-                                message: body_broadcast.message,
-                            }),
-                            msg_id: Some(msg_id),
-                            in_reply_to: None,
-                        },
-                    };
-                    // Ensure that the message is sent at least once
-                    if let Ok(mut msg_queue) = self.msg_queue.write() {
-                        msg_queue.insert(msg_id, message);
-                    }
+        let node_id: Option<String> = {
+            if let Some(node) = self.repo.this_node() {
+                if !node.has_message(body_broadcast.message) {
+                    node.add_message(body_broadcast.message);
+                    Some(node.id())
+                } else {
+                    None
                 }
+            } else {
+                None
+            }
+        };
+        if let Some(node_id) = node_id {
+            let neighbors: Vec<Node> = self.repo.neighbors(&node_id).into_iter().cloned().collect();
+
+            eprintln!("{}:   Broadcasting: {:?}", now_us(), body_broadcast);
+            for neighbor_node in neighbors {
+                if neighbor_node.id() == source.src || neighbor_node.id() == node_id.to_owned() {
+                    continue;
+                }
+                let msg_id = MessageReceiver::get_next_msg_id();
+                let message = Message {
+                    src: node_id.to_owned(),
+                    dest: neighbor_node.id(),
+                    body: Body {
+                        typ: BodyType::Broadcast(BodyBroadcast {
+                            message: body_broadcast.message,
+                        }),
+                        msg_id: Some(msg_id),
+                        in_reply_to: None,
+                    },
+                };
+                // Ensure that the message is sent at least once
+                MessageReceiver::send_outgoing(&message);
+                if let Ok(mut msg_queue) = self.msg_queue.write() {
+                    msg_queue.insert(msg_id, message);
+                }
+                eprintln!("{}:   Queued message: {:?}", now_us(), msg_id);
             }
             return Some(BodyType::BroadcastOk);
         }
+        eprintln!("{}:   Done broadcasting: {:?}", now_us(), body_broadcast);
         None
     }
 
